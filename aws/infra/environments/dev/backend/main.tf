5618f3c24d7571a2c598d948e6c87393062940e8
jlopezl <jose.lopez.lara@outlook.com>
2026-07-14 15:13:22 -0600

    I've added my remote state bucket

--- aws/infra/environments/dev/backend/main.tf
+++ aws/infra/environments/dev/backend/main.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "networking" {
   backend = "s3"
   config = {
-    bucket  = "<YOUR_GENERATED_BUCKET_NAME>"
+    bucket  = "cleanmybelly-tfstate-v1-bb8f23ca"
     key     = "cleanmybelly/infra/shared/networking/terraform.tfstate"
     region  = "us-east-1"
     profile = "terraform-user"
