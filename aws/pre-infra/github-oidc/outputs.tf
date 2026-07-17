3a52ccb0e4d563cf568c80415f694e0207ce5f23
jlopezl <jose.lopez.lara@outlook.com>
2026-07-17 09:04:02 -0600

    I've refactored oicd into /github/..

--- aws/pre-infra/github-oidc/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "github_actions_role_arn" {
-  description = "ARN of the IAM Role created for GitHub Actions OIDC trust. Add this to your GitHub Repository Secrets as AWS_ROLE_TO_ASSUME."
-  value       = aws_iam_role.github_actions_deployer.arn
-}
